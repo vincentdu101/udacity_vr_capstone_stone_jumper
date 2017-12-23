bb0beb7ac81f163d590d06e1ffc558d693d3a365
Vincent Du <vdu@scilearn.com>
2017-12-22 20:27:14 -0800

    adding in player life display and initial calculations

---
+++ Assets/PlayerLifeService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLifeService : MonoBehaviour {
+
+	private double defaultLifeCount = 100.0;
+	private double currentLife = 0.0;
+	private double recoverRate = 0.001;
+	private bool damageTaken = false;
+	private GameObject playerLifeText;
+
+	void Start() {
+		currentLife = defaultLifeCount;
+		playerLifeText = GameObject.FindGameObjectWithTag ("Life");
+	}
+
+	void Update() {
+		if (currentLife < defaultLifeCount && currentLife != 0.0) {
+			currentLife += recoverRate;
+		}
+		playerLifeText.GetComponent<Text> ().text = currentLife + " / " + defaultLifeCount;
+	}
+
+	public void lifeTakingHit() {
+		currentLife -= 1.0;
+		damageTaken = true;
+	}
+
+	public void resetLife() {
+		currentLife = 100.0;
+	}
+
+	public double getLife() {
+		return currentLife;
+	}
+
+
+}
---
+++ Assets/PlayerService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerService : MonoBehaviour {
+
+	private GameObject gameData;
+	private PlayerLifeService playerLifeService; 
+
+	// Use this for initialization
+	void Start () {
+		gameData = GameObject.FindGameObjectWithTag ("GameData");
+		playerLifeService = gameData.GetComponent<PlayerLifeService> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTrigger() {
+
+	}
+}

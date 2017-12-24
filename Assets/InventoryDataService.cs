bcc4808d6629aad39c26b97df3209532a36dc085
Vincent Du <vdu@scilearn.com>
2017-12-23 18:00:04 -0800

    adding initial inventory system

---
+++ Assets/InventoryDataService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDataService : MonoBehaviour {
+
+	private Dictionary<string, bool> orbsCollected;
+	private Dictionary<string, bool> itemsCollected;
+
+	void Start() {
+		orbsCollected = new Dictionary<string, bool> ();
+		itemsCollected = new Dictionary<string, bool> ();
+	}
+
+	public void OrbFound(string color) {
+		orbsCollected.Add (color, true);
+	}
+
+	public bool HaveAllOrbsBeenFound() {
+		return orbsCollected.Keys.Count == 3;
+	}
+
+	public void ItemFound(string item) {
+		itemsCollected.Add (item, true);
+	}
+
+	public bool IsItemFound(string item) {
+		bool found;
+		itemsCollected.TryGetValue (item, out found);
+		return found == true;
+	}
+}
---
+++ Assets/OrbDataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbDataService : MonoBehaviour {
+
+	private Dictionary<string, bool> orbsCollected;
+
+	void Start() {
+		orbsCollected = new Dictionary<string, bool> ();
+	}
+
+	public void OrbFound(string color) {
+		orbsCollected.Add (color, true);
+	}
+
+	public bool HaveAllOrbsBeenFound() {
+		return orbsCollected.Keys.Count == 3;
+	}
+}
